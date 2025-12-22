e89f038f405e4c2074ccbcfa28e01ef7a6ccdc8e
Lukas Pirkl <pirkl.lukas@gmail.com>
2025-12-22 17:36:08 +0100

    Minimal implementation of Clocks and BootRAM to pass the boot

--- Venture/Peripherals/BootRAM.cs
+++ Venture/Peripherals/BootRAM.cs
@@ -17,7 +17,11 @@ public class BootRAM : BytePeripheralBase
 
     protected override byte HandleRead(uint offset)
     {
-        if (offset >= ram.Length)
+        if (offset >= 0x80c && offset <= 0x828)
+        {
+            return 1; // These are bootlocks and I should return something other than zero to signal that the lock was successfully taken
+        }
+        else if (offset >= ram.Length)
         {
             // TODO: There is also BOOTRAM_BASE register on 0x400e0800
             logger.LogWarning("Reading from unhandled offset {offset} of BootRAM", offset.ToHex());
--- Venture/Peripherals/Clocks.cs
+++ Venture/Peripherals/Clocks.cs
@@ -6,6 +6,39 @@ public class Clocks : PeripheralBase
 {
     private readonly ILogger<Clocks> logger;
 
+
+
+    private const uint CLK_REF_CTRL = 0x30;
+    private const uint CLK_REF_DIV = 0x34;
+    private const uint CLK_REF_SELECTED = 0x38;
+
+    enum RefSource
+    {
+        ROSC_CLKSRC_PH = 0x0,
+        CLKSRC_CLK_REF_AUX = 0x1,
+        XOSC_CLKSRC = 0x2,
+        LPOSC_CLKSRC = 0x3
+    }
+
+    private RefSource refSource = RefSource.ROSC_CLKSRC_PH;
+
+
+
+
+    private const uint CLK_SYS_CTRL = 0x3c;
+    private const uint CLK_SYS_DIV = 0x40;
+    private const uint CLK_SYS_SELECTED = 0x44;
+
+    enum SysSource
+    {
+        CLK_REF = 0x0,
+        CLKSRC_CLK_SYS_AUX = 0x1,
+    }
+
+    private SysSource sysSource = SysSource.CLK_REF;
+
+
+
     public Clocks(ILogger<Clocks> logger) 
         : base(0x40010000)
     {
@@ -16,26 +49,28 @@ public class Clocks : PeripheralBase
     {
         logger.LogWarning("Reading from unhandled offset {offset}", offset.ToHex());
 
-        if (offset == 0x34) // CLK_REF_DIV Register
+        if (offset == CLK_REF_DIV)
         {
             logger.LogWarning("Reading CLK_REF_DIV register from Clocks");
             return 0x00010000;
         }
-        else if (offset == 0x38) // CLK_REF_SELECTED Register
+        else if (offset == CLK_REF_SELECTED)
         {
             logger.LogWarning("Reading CLK_REF_SELECTED register from Clocks");
-            return 0x00000004;
+            return (uint)(1 << (int)refSource);
         }
-        else if (offset == 0x40) //  CLK_SYS_DIV Register
+
+        else if (offset == CLK_SYS_DIV)
         {
             logger.LogWarning("Reading  CLK_SYS_DIV register from Clocks");
             return 0x00010000;
         }
-        else if (offset == 0x44) // CLK_SYS_SELECTED Register
+        else if (offset == CLK_SYS_SELECTED)
         {
             logger.LogWarning("Reading CLK_SYS_SELECTED register from Clocks");
-            return 0x00000002;
+            return (uint)(1 << (int)sysSource);
         }
+        
         else if (offset == 0x4c) // CLK_PERI_DIV Register
         {
             logger.LogWarning("Reading CLK_PERI_DIV register from Clocks");
@@ -63,5 +98,15 @@ public class Clocks : PeripheralBase
     protected override void HandleWrite(uint offset, uint value)
     {
         logger.LogWarning("Writing to unhandled offset {offset} data {data}", offset.ToHex(), value.ToHex());
+
+        if (offset == CLK_REF_CTRL)
+        {
+            refSource = (RefSource)value.ExtractBits(0, 2);
+        }
+
+        else if (offset == CLK_SYS_CTRL)
+        {
+            sysSource = (SysSource)value.ExtractBits(0, 1);
+        }
     }
 }

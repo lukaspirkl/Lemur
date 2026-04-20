b89c166f605711f439911b21bddb961c387da019
Lukas Pirkl <pirkl.lukas@gmail.com>
2026-04-20 20:43:03 +0200

    Remove unused code and Ursa library
    I don't want additional dependency to have everything smaller - simpler

--- VentureUI/Peripherals/PinDevicesView.axaml.cs
+++ VentureUI/Peripherals/PinDevicesView.axaml.cs
@@ -1,8 +1,5 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Ursa.Common;
-using Ursa.Controls;
-using Ursa.Controls.Options;
 
 namespace VentureUI;
 
@@ -11,19 +8,5 @@ public partial class PinDevicesView : UserControl
     public PinDevicesView()
     {
         InitializeComponent();
-    }
-
-    private void OnHelpClick(object? sender, RoutedEventArgs e)
-    {
-        Drawer.ShowCustom<PinDevicesHelpView, PinDevicesHelpViewModel>(
-            new PinDevicesHelpViewModel(),
-            options: new DrawerOptions
-            {
-                Position         = Position.Right,
-                Title            = "Pin Device Naming",
-                IsCloseButtonVisible = true,
-                CanLightDismiss  = true,
-                MinWidth         = 380,
-            });
     }
 }
